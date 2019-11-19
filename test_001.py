b8ba6f4550bc874b87b4edcebd0d34fd6fc752e1
Administrator <1558570566@qq.com>
2019-11-19 17:34:56 +0800

    1234562

--- test_001.py
+++ test_001.py
@@ -6,7 +6,7 @@ class Test_allure:
         pass
     @pytest.mark.parametrize("a",[1,2,3])
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
-    @allure.step('我是测试步骤001')
+    @allure.step('我是测试步骤这里做出改变001')
     def test_al(self, a):
         allure.attach('描述', '我是测试步骤001的描述～～～')
         assert a != 2
